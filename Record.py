8a8539c1ca46f625cb28199aecf2150a5d3b560f
samueltenka <iguavadon@gmail.com>
2014-09-06 18:33:09 -0700

    Update Record.py
    Vector arithmetic.

--- Record.py
+++ Record.py
@@ -10,24 +10,29 @@ make_range = lambda dur: range(to_discrete_ind(dur))
 class Record:
    def __init__(self, filename='', source=None, duration=0.0, freq=0.0):
       if filename:
-         self.read_from(filename)
+         self.read_from(filename, duration)
       elif source:
-         self.copy_from(source)
+         if type(source) == type([]):
+            self.copy_from(source)
+         else:
+            self.copy_from(source.amps)
       elif duration:
          if freq:
             self.generate_sin(duration, freq)
          else:
             self.generate_silence(duration)
 
-   def copy_from(self, other):
-      self.duration = other.duration
-      self.amps = other.amps[:]
-   def read_from(self, filename):
+   def copy_from(self, source):
+      self.amps = source[:]
+      self.duration = duration_of(self.amps)
+   def read_from(self, filename, duration=0):
       ''' reads mono PCM16 '''
       ''' thx to http://stackoverflow.com/questions/2060628/how-to-read-wav-file-in-python '''
       self.amps = []
       with wave.open(filename, 'r') as waveFile:
          length = waveFile.getnframes()
+         if duration:
+            length = min(length, to_discrete_ind(duration))
          for i in range(0,length):
             waveData = waveFile.readframes(1)
             data = struct.unpack("<h", waveData)
@@ -46,13 +51,30 @@ class Record:
         for a in self.amps:
           waveFile.writeframes(struct.pack('h', int(a*32767)))
         
+   def sub(self, start, dur):
+      return Record(source=self.amps[to_discrete_ind(start):to_discrete_ind(start+dur)])
+   def reverse(self):
+      self.amps = self.amps[::-1]
+   def join_from(self, subs):
+      self.amps = []
+      self.duration = 0.0
+      for s in subs:
+         self.amps += s.amps
+      self.duration = duration_of(self.amps)
+
    def dot(self, other):
       if self.duration == other.duration:
          return sum(mine*theirs for mine,theirs in zip(self.amps, other.amps))
+   def times(self, num):
+      self.amps = [a*num for a in self.amps]
+   def plus(self, other):
+      if self.duration == other.duration:
+         return Record([mine+theirs for mine,theirs in zip(self.amps, other.amps)])
+      
 
     
-R = Record(duration=1.0, freq=2*pi*440)
-print(R.dot(R)) ## get 22050 = 44100/2, correct
+#R = Record(duration=1.0, freq=2*pi*440)
+#print(R.dot(R)) ## get 22050 = 44100/2, correct
 
-S = Record(filename='C:\\Users\\Sam\\Desktop\\MHacks\\sam_ahh_0414.wav')
-R.write_to('hi.wav')
+#S = Record(filename='C:\\Users\\Sam\\Desktop\\MHacks\\sam_ahh_0414.wav')
+#R.write_to('hi.wav')
