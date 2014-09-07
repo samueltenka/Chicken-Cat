f57ff1dec6016e11abbdb55fafb0863319b9b38c
samueltenka <iguavadon@gmail.com>
2014-09-06 18:49:22 -0700

    Update Orthogonalizer.py
    currently broken orthonormalization procedure

--- Orthogonalizer.py
+++ Orthogonalizer.py
@@ -1,5 +1,6 @@
-from Record import Record
+from Record_04 import Record
 
+'''read control and target samples, 2 seconds each:'''
 sound_names = ['ahh',
                'ooh',
                'eeh',
@@ -12,6 +13,24 @@ sound_names = ['ahh',
                'ung']
 training_pairs = {}
 for name in sound_names:
-   training_pairs[name] = (Record(duration=2.0, filename='data\\'+name.upper()+'220_brian.wav'),
-                           Record(duration=2.0, filename='data\\'+name.upper()+'220_sam.wav'))
+   training_pairs[name] = (Record(duration=0.2, filename='data\\'+name.upper()+'220_brian.wav'),
+                           Record(duration=0.2, filename='data\\'+name.upper()+'220_sam.wav'))
    print('read', name)
+
+''' orthonormalize control samples (transforming target samples along for the ride '''
+ons = {}
+for name in sound_names:
+   c, t = training_pairs[name]
+   ## todo: normalize c,t to begin with, so brian doesn't speak louder than sam, etc.
+   '''orthogonalize:'''
+   oc, ot = c, t
+   for prev in sound_names:
+      if prev==name:
+         break
+      pc = ons[prev][0]
+      coeff = c.dot(pc)
+      oc = oc.minus(pc.times(coeff)); ot = ot.minus(pc.times(coeff))
+   '''normalize:'''
+   onc = oc.times(oc.dot(oc)**(-0.5)); ont = ot.times(oc.dot(oc)**(-0.5))
+   ons[name] = (onc, ont)
+   print('orthonormalized', name)
