3c5046d83ecf320cc371c3bcdbc059800abdaed9
samueltenka <iguavadon@gmail.com>
2014-09-06 19:28:11 -0700

    Create Translator.py
    "main": translates from inputted_brian_voice to generated_sam_voice

---
+++ Translator.py
@@ -0,0 +1,26 @@
+from Record_05 import Record
+from Orthogonalizer_05 import sound_names, ons
+
+''' read & segment: '''
+v = Record(filename='inputted_brian_voice.wav')
+segs = int(v.duration*10)
+starts = [i*0.1 for i in range(segs)]
+vsubs = [v.sub(t, 0.1) for t in starts]
+
+''' translate '''
+wsubs = []
+for vs in subs:
+   ws = Record(duration=0.1)
+   error = Record(vs)
+   for name in sound_names:
+      c, t = ons[name]
+      coeff = vs.smart_dot(c)
+      ws = ws.plus(t.times(coeff))
+      error = error.minus(c.times(coeff))
+   ws = ws.plus(error)
+   wsubs.append(ws)
+
+''' join & write: '''
+w = Record()
+w.join_from(wsubs)
+w.write_to('generated_sam_voice.wav')
